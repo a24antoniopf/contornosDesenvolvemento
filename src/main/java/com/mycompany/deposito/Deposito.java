8c030af239671233328457f51df35e4c15dc038b
a24antoniopf <anicolaspf@gmail.com>
2025-02-16 23:09:27 +0100

    javadoc

--- src/main/java/com/mycompany/deposito/Deposito.java
+++ src/main/java/com/mycompany/deposito/Deposito.java
@@ -1,16 +1,21 @@
 package com.mycompany.deposito;
 import cuentas.*;
-
+/**
+ * Clase que representa deposito.
+ * @author nemerpus
+ */
 public class Deposito {
     public static void main(String[] args) {
         CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         operativa_cuenta(cuenta1, 500);
     }
-
+// Método Operativa Cuenta
     public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+        // Instacia objeto cuenta1
         double saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es " + saldoActual);
-
+        
+// Funcionamiento cuenta 
         try {
             cuenta1.retirar(2300);
             System.out.println("Se han retirado 2300 euros");
--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,28 +1,32 @@
 package cuentas;
-
+/**
+ * Clase que representa una cuenta bancaria.
+ * @author nemerpus
+ */
 public class CCuenta {
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInteres;
-
+    
+/* Constructor CCuenta*/
     public CCuenta(String nombre, String cuenta, double saldo, double tipoInteres) {
         this.nombre = nombre;
         this.cuenta = cuenta;
         this.saldo = saldo;
         this.tipoInteres = tipoInteres;
     }
-
+//Método estado saldo
     public double estado(){
         return saldo;
     }
-
+//Método ingresar
     public void ingresar(double cantidad) throws Exception{
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo += cantidad;
     }
-
+// Método retirar
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
